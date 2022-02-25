832c0914559d1665642996af484b18b08930c161
markmead <itsmarkmead@gmail.com>
2022-02-25 19:41:55 +0000

    fix(components): Layout import path

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import "../styles/globals.css";
 
 import Head from "next/head";
 
-import Layout from "../components/layout";
+import Layout from "../components/Layout";
 
 function MyApp({ Component, pageProps }) {
   return (
