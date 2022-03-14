5ce7e49fb75098f3ee27a464c8074ff0b6d20a19
markmead <itsmarkmead@gmail.com>
2022-03-14 12:18:30 +0000

    fix/site/example-card

--- components/Card.js
+++ components/Card.js
@@ -3,18 +3,17 @@ import Link from "next/link";
 export default function Card({ title, description, path, emoji }) {
   return (
     <Link as={`/examples/${path}`} href={`/examples/[slug]`}>
-      <a class="relative block group h-56">
-        <div class="relative flex items-end h-full transition bg-white border-4 border-black group-hover:-translate-x-2 group-hover:-translate-y-2 rounded-3xl group-hover:shadow-offset shadow-black">
-          <div class="px-8 pb-8 group-hover:opacity-0 group-hover:absolute">
-            <span className="text-4xl" role="img" aria-hidden="true">
+      <a class="block h-56 group" href="/blog">
+        <div class="relative flex items-end h-full transition bg-white border-4 border-black group-hover:-translate-x-2 group-hover:-translate-y-2 rounded-3xl group-hover:shadow-[8px_8px_0_0_#000] p-8">
+          <div class="lg:group-hover:opacity-0 lg:group-hover:absolute">
+            <span class="text-3xl sm:text-4xl" role="img" aria-hidden="true">
               {emoji}
             </span>
-
-            <h2 class="mt-4 text-2xl font-bold">{title}</h2>
+            <p class="mt-4 text-xl font-bold sm:text-2xl">{title}</p>
           </div>
 
-          <div class="absolute p-8 opacity-0 group-hover:opacity-100 group-hover:relative">
-            <h2 class="mt-4 text-2xl font-bold">{title}</h2>
+          <div class="absolute opacity-0 lg:group-hover:opacity-100 lg:group-hover:relative">
+            <p class="text-2xl font-bold">{title}</p>
 
             <p class="mt-4 text-lg font-medium leading-relaxed">
               {description}
@@ -23,20 +22,5 @@ export default function Card({ title, description, path, emoji }) {
         </div>
       </a>
     </Link>
-    // <Link as={`/examples/${path}`} href={`/examples/[slug]`}>
-    //   <a className="relative block group rounded-xl">
-    //     <span className="absolute inset-0 border-2 border-black border-dashed pointer-events-none"></span>
-
-    //     <div className="transition bg-white border-2 border-black group-hover:-translate-x-2 group-hover:-translate-y-2">
-    //       <div className="px-8 pt-24 pb-8 transition group-hover:opacity-0 group-hover:absolute">
-    //         <span className="text-3xl" aria-hidden="true" role="img">
-    //           {emoji}
-    //         </span>
-
-    //         <h2 className="mt-4 text-2xl font-bold">{title}</h2>
-    //       </div>
-    //     </div>
-    //   </a>
-    // </Link>
   );
 }
