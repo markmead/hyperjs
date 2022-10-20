ef981bd12ad1c70a6af59401bc6e56f4ba41ed3e
Mark Mead <itsmarkmead@gmail.com>
2022-10-20 15:58:00 +0100

    Remove dribbble

--- components/Footer.js
+++ components/Footer.js
@@ -8,7 +8,7 @@ export default function Footer() {
               className="text-blue-500 hover:text-opacity-75"
               href="https://twitter.com/itsmarkmead"
               target="_blank"
-              rel="noopener noreferrer"
+              rel="noreferrer"
               aria-label="Twitter"
             >
               <svg
@@ -25,7 +25,7 @@ export default function Footer() {
               className="text-gray-900 hover:text-opacity-75"
               href="https://github.com/markmead"
               target="_blank"
-              rel="noopener noreferrer"
+              rel="noreferrer"
               aria-label="GitHub"
             >
               <svg
@@ -41,27 +41,6 @@ export default function Footer() {
                 />
               </svg>
             </a>
-
-            <a
-              className="text-pink-600 hover:text-opacity-75"
-              href="https://dribbble.com/markmead"
-              target="_blank"
-              rel="noopener noreferrer"
-              aria-label="Dribbble"
-            >
-              <svg
-                className="w-8 h-8"
-                fill="currentColor"
-                viewBox="0 0 24 24"
-                aria-hidden="true"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10c5.51 0 10-4.48 10-10S17.51 2 12 2zm6.605 4.61a8.502 8.502 0 011.93 5.314c-.281-.054-3.101-.629-5.943-.271-.065-.141-.12-.293-.184-.445a25.416 25.416 0 00-.564-1.236c3.145-1.28 4.577-3.124 4.761-3.362zM12 3.475c2.17 0 4.154.813 5.662 2.148-.152.216-1.443 1.941-4.48 3.08-1.399-2.57-2.95-4.675-3.189-5A8.687 8.687 0 0112 3.475zm-3.633.803a53.896 53.896 0 013.167 4.935c-3.992 1.063-7.517 1.04-7.896 1.04a8.581 8.581 0 014.729-5.975zM3.453 12.01v-.26c.37.01 4.512.065 8.775-1.215.25.477.477.965.694 1.453-.109.033-.228.065-.336.098-4.404 1.42-6.747 5.303-6.942 5.629a8.522 8.522 0 01-2.19-5.705zM12 20.547a8.482 8.482 0 01-5.239-1.8c.152-.315 1.888-3.656 6.703-5.337.022-.01.033-.01.054-.022a35.318 35.318 0 011.823 6.475 8.4 8.4 0 01-3.341.684zm4.761-1.465c-.086-.52-.542-3.015-1.659-6.084 2.679-.423 5.022.271 5.314.369a8.468 8.468 0 01-3.655 5.715z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </a>
           </div>
 
           <nav className="relative flex flex-wrap justify-center gap-8 p-8 text-sm font-bold border-4 border-black rounded-xl">
@@ -69,7 +48,7 @@ export default function Footer() {
               className="hover:opacity-75"
               href="https://hyperui.dev"
               target="_blank"
-              rel="noopener noreferrer"
+              rel="noreferrer"
             >
               HyperUI
             </a>
@@ -78,7 +57,7 @@ export default function Footer() {
               className="hover:opacity-75"
               href="https://hypercolor.dev"
               target="_blank"
-              rel="noopener noreferrer"
+              rel="noreferrer"
             >
               Hypercolor
             </a>
@@ -87,7 +66,7 @@ export default function Footer() {
               className="hover:opacity-75"
               href="https://hyperjs.dev"
               target="_blank"
-              rel="noopener noreferrer"
+              rel="noreferrer"
             >
               HyperJS
             </a>
@@ -96,7 +75,7 @@ export default function Footer() {
               className="hover:opacity-75"
               href="https://markmead.dev"
               target="_blank"
-              rel="noopener noreferrer"
+              rel="noreferrer"
             >
               Portfolio
             </a>
@@ -112,7 +91,7 @@ export default function Footer() {
               className="block mt-1 text-pink-600 underline decoration-wavy decoration-pink-500 hover:opacity-75"
               href="https://alpinejs.dev/components"
               target="_blank"
-              rel="noopener noreferrer"
+              rel="noreferrer"
             >
               Alpine JS Components
             </a>
@@ -124,5 +103,5 @@ export default function Footer() {
         </div>
       </div>
     </footer>
-  );
+  )
 }
