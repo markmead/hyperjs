a0ce3e3d8111625580d75daf11641b314780aec2
Mark Mead <itsmarkmead@gmail.com>
2022-12-31 17:42:20 +0000

    Remove links to my Twitter

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -1,20 +1,8 @@
 import IconGithub from '@/components/IconGithub'
-import IconTwitter from '@/components/IconTwitter'
 
 export default function Social() {
   return (
-    <div className="flex items-center gap-4">
-      <a
-        href="https://twitter.com/itsmarkmead"
-        target="_blank"
-        rel="noreferrer"
-        className="text-white transition hover:text-white/75"
-      >
-        <span className="sr-only">Twitter</span>
-
-        <IconTwitter className="w-6 h-6" />
-      </a>
-
+    <div>
       <a
         href="https://github.com/markmead/hyperjs"
         target="_blank"
