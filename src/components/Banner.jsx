2f290db7e7cffa7eea2df5c4f9ede85076ea4c49
Mark Mead <itsmarkmead@gmail.com>
2023-06-19 09:14:33 +0100

    Initial commit

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,18 +1,14 @@
-export default function Banner() {
+export default function Banner({ bannerTitle, bannerText }) {
   return (
     <section>
       <div className="max-w-screen-xl px-4 py-16 mx-auto">
-        <div className="max-w-xl">
+        <div className="mx-auto text-center max-w-prose">
           <h1 className="text-3xl font-black text-white sm:text-5xl">
-            Enjoy the DOM
-            <br />
-            with <em className="mt-1 not-italic text-teal-400">Alpine JS</em> 💫
+            {bannerTitle}
           </h1>
 
           <p className="mt-4 font-medium text-slate-300 sm:leading-relaxed sm:text-xl">
-            Alpine JS allows you to write DOM manipulation all in your HTML,
-            Liquid, Blade etc. Here is a collection of components that you can
-            copy and paste into your project.
+            {bannerText}
           </p>
         </div>
       </div>
