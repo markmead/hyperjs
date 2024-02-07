f06272581fa9afb420d95374de337f41005ed5b1
Mark Mead <itsmarkmead@gmail.com>
2024-02-07 09:09:50 +0000

    Initial commit

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -57,7 +57,7 @@ export default function Preview({ componentId, componentTitle }) {
           >
             <span className="text-sm font-medium">{buttonText}</span>
 
-            <ClipboardIcon className="h-4 w-4" />
+            <ClipboardIcon className="size-4" />
           </button>
         </div>
 
