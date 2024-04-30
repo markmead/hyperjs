c1d9a53f92c2e79e900a286ed54eb09c6ecfd914
Mark Mead <itsmarkmead@gmail.com>
2024-04-30 10:58:48 +0100

    Add the CDN

--- src/utils/transformers.js
+++ src/utils/transformers.js
@@ -2,11 +2,34 @@ export function componentPreviewHtml(componentHtml) {
   return `
       <html>
         <head>
-          <link rel="stylesheet" href="/components.css">
-
           <link rel="preconnect" href="https://fonts.googleapis.com">
           <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
           <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap" rel="stylesheet">
+
+          <script src="https://cdn.tailwindcss.com?plugins=forms"></script>
+
+          <script>
+            tailwind.config = {
+              theme: {
+                extend: {
+                  animation: {
+                    'slide-in': 'slide-in 0.15s ease-in forwards',
+                    'slide-out': 'slide-out 0.15s ease-in forwards',
+                  },
+                  keyframes: {
+                    'slide-in': {
+                      '0%': { transform: 'translateX(100%)' },
+                      '100%': { transform: 'translateX(0)' },
+                    },
+                    'slide-out': {
+                      '0%': { transform: 'translateX(0)' },
+                      '100%': { transform: 'translateX(100%)' },
+                    },
+                  },
+                },
+              },
+            }
+          </script>
         </head>
 
         <body class="font-sans antialiased p-4">
