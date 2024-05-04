8f3aa03d39632305f09a04f869f0671b15b6d317
Mark Mead <itsmarkmead@gmail.com>
2024-05-04 09:03:49 +0100

    Remove ads

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,8 +1,5 @@
 import Head from 'next/head'
 
-import { useEffect } from 'react'
-import { usePathname } from 'next/navigation'
-
 import 'prismjs/themes/prism-okaidia.css'
 import '@style/site.css'
 
@@ -15,21 +12,6 @@ export default function MyApp({ Component, pageProps }) {
       </Head>
 
       <Component {...pageProps} />
-
-      <Ads />
     </>
   )
-}
-
-function Ads() {
-  const routerPathname = usePathname()
-
-  useEffect(() => {
-    const newScript = document.createElement('script')
-
-    newScript.src = 'https://media.ethicalads.io/media/client/ethicalads.min.js'
-    newScript.async = true
-
-    document.body.appendChild(newScript)
-  }, [routerPathname])
 }
